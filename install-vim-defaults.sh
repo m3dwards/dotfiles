abe66b0a10f715767e63610051061be90cc14d9b
Max Edwards <maxedwar@thoughtworks.com>
2014-12-05 22:23:02 +0000

    added ctrlspace

--- install-vim-defaults.sh
+++ install-vim-defaults.sh
@@ -3,3 +3,6 @@ git clone git://github.com/tpope/vim-sensible.git
 
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
+
+cd ~/.vim/bundle
+git clone https://github.com//szw/vim-ctrlspace.git
