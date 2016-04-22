27f53a68a03a6ddd97fd3935956696a49c874793
Max Edwards <max.edwards@stockport.gov.uk>
2016-04-22 14:17:36 +0100

    added new install all script

---
+++ install-all.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+./install-bashrc.sh
+
+./install-vimrc.sh
+./install-vim-defaults.sh
+./install-pathogen.sh
--- install-vim-defaults.sh
+++ install-vim-defaults.sh
@@ -1,5 +1,9 @@
+#! /bin/bash
+
+mkdir -p ~/.vim/bundle
+
 cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-sensible.git
+git clone git@github.com:tpope/vim-sensible.git
 
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
