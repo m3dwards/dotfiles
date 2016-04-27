02cf7f0c46ef5625152bac3ab588dd555c58835c
Max Edwards <max.edwards@stockport.gov.uk>
2016-04-27 09:53:25 +0100

    updated some vim plugins

--- install-vim-defaults.sh
+++ install-vim-defaults.sh
@@ -8,8 +8,14 @@ git clone git@github.com:tpope/vim-sensible.git
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 
-# cd ~/.vim/bundle
-# git clone https://github.com/szw/vim-ctrlspace.git
+cd ~/.vim/bundle
+git clone git@github.com:ctrlpvim/ctrlp.vim.git
 
 cd ~/.vim/bundle
 git clone git@github.com:vim-scripts/ReplaceWithRegister.git
+
+cd ~/.vim/bundle
+git clone git@github.com:tpope/vim-surround.git
+
+cd ~/.vim/bundle
+git clone git@github.com:tpope/vim-commentary.git
