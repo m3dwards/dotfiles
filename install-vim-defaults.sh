09a30b85123240371ca775af505fb23978349b3f
maxwedwards <youwontforgetthis@gmail.com>
2016-05-03 18:05:02 +0100

    Update install-vim-defaults.sh

--- install-vim-defaults.sh
+++ install-vim-defaults.sh
@@ -3,19 +3,19 @@
 mkdir -p ~/.vim/bundle
 
 cd ~/.vim/bundle
+
 git clone git@github.com:tpope/vim-sensible.git
 
-cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 
-cd ~/.vim/bundle
 git clone git@github.com:ctrlpvim/ctrlp.vim.git
 
-cd ~/.vim/bundle
 git clone git@github.com:vim-scripts/ReplaceWithRegister.git
 
-cd ~/.vim/bundle
 git clone git@github.com:tpope/vim-surround.git
 
-cd ~/.vim/bundle
 git clone git@github.com:tpope/vim-commentary.git
+
+git clone git@github.com:kana/vim-textobj-user.git
+
+git clone git@github.com:bps/vim-textobj-python.git
